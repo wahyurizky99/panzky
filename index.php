0f0b6e8780e81485b98f4517940f81bcc8d3cf11
unknown <panjiyumadana5@gmail.com>
2019-10-01 17:39:39 +0700

    File Baru

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<script src="bootstrap/js/jquery-3.3.1.slim.min.js"></script>
+	<script src="bootstrap/js/propper.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+
+    <title>Tugas Git PANZKY</title>		
+</head>
+<body>
+
+</body>
+</html>
